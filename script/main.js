1ef4fb73ea26493c595bcd23808b6d73efd74686
HAMAN23333 <xiamin2333@gmail.com>
2022-05-12 20:21:40 +0800

    first commit

---
+++ script/main.js
@@ -0,0 +1,33 @@
+document.querySelector('img').onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'image/logo.eb1324e44442.svg') {
+        myImage.setAttribute('src', 'image/Google_Chrome_icon_(February_2022).svg.png');
+    } else {
+        myImage.setAttribute('src', 'image/logo.eb1324e44442.svg');
+    }
+}
+
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World!';
+
+let myButton = document.querySelector('button');
+function setUserName() {
+    let myName = prompt('请输入你的名字');
+    if(!myName) {
+        setUserName()
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla 酷毙了, ' + myName;
+    }
+}
+myButton.onclick = function() {
+    setUserName();
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了, ' + storedName;
+}
+
+
